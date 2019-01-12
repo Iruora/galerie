a0cd339c3cde091c591f65f331082446b47da769
Nidhal Arouri <arrouurii@gmail.com>
2019-01-12 04:48:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+//include the S3 class              
+if (!class_exists('S3'))require_once('S3.php');
+ 
+//AWS access info
+if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAIULOWXCHMZAQLIJQ');
+if (!defined('awsSecretKey')) define('awsSecretKey', '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');
+ 
+//instantiate the class
+$s3 = new S3(awsAccessKey, awsSecretKey);
+ 
+//we'll continue our script from here in step 4!
+ 
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -48,8 +62,7 @@
 	</div>
 </div>
 <?php
-	//include the S3 class              
-	if (!class_exists('S3'))require_once('S3.php');
+	//include the S3 class             
 	try
 	{
 		$param='mysql:host=mydb.cziozwt7vuhe.eu-west-1.rds.amazonaws.com;dbname=galerie';
