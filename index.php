a68d707c1958b22efee7b2894b4d324e8b616770
Nidhal Arouri <arrouurii@gmail.com>
2019-01-12 06:22:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,14 +17,7 @@
 <body>
 	<?php
 //include the S3 class              
-if (!class_exists('S3'))require_once('S3.php');
- 
-//AWS access info
-if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAIULOWXCHMZAQLIJQ');
-if (!defined('awsSecretKey')) define('awsSecretKey', '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');
- 
-//instantiate the class
-$s3 = new S3(awsAccessKey, awsSecretKey);
+
  
 //we'll continue our script from here in step 4!
  
@@ -84,8 +77,15 @@ $s3 = new S3(awsAccessKey, awsSecretKey);
 			$nomimg = str_replace('@', '_', $nomimg);
 	    
 	    		$tmpfilename = $_FILES['img']['tmp_name'] ;
-	                echo "S3.putBucket = ";
-	    		var_dump($s3->putBucket("ndhs3", S3::ACL_PUBLIC_READ));
+	                if (!class_exists('S3'))require_once('S3.php');
+ 
+			//AWS access info
+			if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAIULOWXCHMZAQLIJQ');
+			if (!defined('awsSecretKey')) define('awsSecretKey', '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');
+ 
+			//instantiate the class
+			$s3 = new S3(awsAccessKey, awsSecretKey);
+	    		//var_dump($s3->putBucket("ndhs3", S3::ACL_PUBLIC_READ));
 			//move the file
 			if ($s3->putObjectFile($tmpfilename, "ndhs3", $nomimg, S3::ACL_PUBLIC_READ)) {
 				echo "<strong>S3::We successfully uploaded your file.</strong>";
